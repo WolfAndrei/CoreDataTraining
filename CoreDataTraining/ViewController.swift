808f242fb4b7d9690ba9a5ec8179e852188aac0f
Andrei Volkau <101.andrei.wolf@mail.ru>
2020-08-13 14:08:23 +0300

    Initial Commit

---
+++ CoreDataTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataTraining
+//
+//  Created by Andrei Volkau on 13.08.2020.
+//  Copyright © 2020 Andrei Volkau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
